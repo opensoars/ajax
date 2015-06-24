80ceb3ee23fc34103248073f9dd68ca1ac65f70a
unknown <sam_1700@hotmail.com>
2015-06-24 12:28:37 +0200

    Better code style/doc

--- ajax.js
+++ ajax.js
@@ -1,38 +1,40 @@
 /**
  * Ajax constructor.
  * @param {object} o - Options
+ * @return {object} this - Return itself, allows for chain calling
  * @example
  * new Ajax({ url: '/' }).done(cb).fail(cb);
  */
-function Ajax(o){
+function Ajax(o) {
   var self = this;
   o = o || {};
 
-  if(!o.url) throw 'Ajax needs a url to make the request to';
-
-  o.method  = o.method  || 'GET';
-  o.data    = o.data    || '';
-  o.timeout = o.timeout || 5000;
-
-
-  this.doneCb = undefined;
-  this.failCb = undefined;
-
+  if(!o.url)
+    throw 'Ajax needs a url to make the request to';
 
   var req = new XMLHttpRequest(),
       has_completed = false;
+
+  o.method = o.method || 'GET';
+  o.data = o.data || '';
+  o.timeout = o.timeout || 5000;
+
+  this.doneCb = undefined;
+  this.failCb = undefined;
 
   /**
    * Gets called when a requests succeeds.
    * @param {string|object} res - Response string, object if JSON
    * @private
    */
-  function done(res){
+  function done(res) {
     if(has_completed === false){
       has_completed = true;
-      if(self.doneCb) self.doneCb(res, req); 
+      if(self.doneCb)
+        self.doneCb(res, req); 
     }
-    else throw '`has_completed === true`. Already called done or fail';
+    else
+      throw '`has_completed === true`. Already called done or fail';
   }
 
   /**
@@ -40,19 +42,20 @@ function Ajax(o){
    * @param {object} res - Response object describing the failure
    * @private
    */
-  function fail(res){
+  function fail(res) {
     if(has_completed === false){
       has_completed = true;
       if(self.failCb) self.failCb(res, req); 
     }
-    else throw '`has_completed === true`. Already called done or fail';
+    else
+      throw '`has_completed === true`. Already called done or fail';
   }
 
   /**
    * Gets called when the specified timeout is exceeded.
    * @private
    */
-  function ontimeout(){
+  function ontimeout() {
     fail({
       desc: 'Request timed out after: ' + req.timeout + ' ms',
       status: this.status
@@ -65,7 +68,7 @@ function Ajax(o){
    * @this XMLHttpRequest
    * @private
    */
-  function onreadystatechange(){
+  function onreadystatechange() {
     if(this.readyState === 4 && this.status !== 0){
       if(this.status === 200 || this.status === 304){
 
@@ -82,39 +85,35 @@ function Ajax(o){
     }
   };
 
-
-  // Bind event handler functions to req
   req.onreadystatechange = onreadystatechange 
   req.ontimeout = ontimeout;
 
-
   req.open(o.method, o.url, true);
 
-  // Timeout has to be set AFTER req.open cuz of IE o.O
+  // Timeout has to be set AFTER req.open cuz of IE.
   req.timeout = o.timeout;
 
-  // Make sure data is OK
+  // Make sure data is OK, we can't send functions and want to
+  // stringify the data if it's JSON.
   if(typeof o.data === 'function')
     throw 'Ajax cannot send a function';
 
   if(typeof o.data === 'object')
     o.data = JSON.stringify(o.data);
 
-
   req.send(o.data);
-
 
   return this;
 }
 
 
-Ajax.prototype.done = function (cb){
+Ajax.prototype.done = function (cb) {
   this.doneCb = cb;
   return this;
 };
 
 
-Ajax.prototype.fail = function (cb){
+Ajax.prototype.fail = function (cb) {
   this.failCb = cb;
   return this;
 };
